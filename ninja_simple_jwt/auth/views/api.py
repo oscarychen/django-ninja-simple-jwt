8423c39865f33bcd492cfba85b3a86f475eb0bd5
Oscar Chen <oscar.y.chen@gmail.com>
2024-01-14 00:10:46 -0700

    Fix User.last_login bug with login endpoints. add unit test coverage

--- ninja_simple_jwt/auth/views/api.py
+++ ninja_simple_jwt/auth/views/api.py
@@ -25,19 +25,21 @@ mobile_auth_router = Router()
 web_auth_router = Router()
 
 
-@mobile_auth_router.post("/sign-in", response=MobileSignInResponse)
+@mobile_auth_router.post("/sign-in", response=MobileSignInResponse, url_name="mobile_signin")
 def mobile_sign_in(request: HttpRequest, payload: SignInRequest) -> dict:
     payload_data = payload.dict()
     user = authenticate(username=payload_data["username"], password=payload_data["password"])
-    user_logged_in.send(sender=user.__class__, request=request, user=user)
+
     if user is None:
         raise AuthenticationError()
+
+    user_logged_in.send(sender=user.__class__, request=request, user=user)
     refresh_token, _ = get_refresh_token_for_user(user)
     access_token, _ = get_access_token_for_user(user)
     return {"refresh": refresh_token, "access": access_token}
 
 
-@mobile_auth_router.post("/token-refresh", response=MobileTokenRefreshResponse)
+@mobile_auth_router.post("/token-refresh", response=MobileTokenRefreshResponse, url_name="mobile_token_refresh")
 def mobile_token_refresh(request: HttpRequest, payload: MobileTokenRefreshRequest) -> dict:
     payload_data = payload.dict()
     try:
@@ -48,13 +50,15 @@ def mobile_token_refresh(request: HttpRequest, payload: MobileTokenRefreshReques
     return {"access": access_token}
 
 
-@web_auth_router.post("/sign-in", response=WebSignInResponse)
+@web_auth_router.post("/sign-in", response=WebSignInResponse, url_name="web_signin")
 def web_sign_in(request: HttpRequest, payload: SignInRequest, response: HttpResponse) -> dict:
     payload_data = payload.dict()
     user = authenticate(username=payload_data["username"], password=payload_data["password"])
-    user_logged_in.send(sender=user.__class__, request=request, user=user)
+
     if user is None:
         raise AuthenticationError()
+
+    user_logged_in.send(sender=user.__class__, request=request, user=user)
     refresh_token, refresh_token_payload = get_refresh_token_for_user(user)
     access_token, _ = get_access_token_for_user(user)
     response.set_cookie(
@@ -69,7 +73,7 @@ def web_sign_in(request: HttpRequest, payload: SignInRequest, response: HttpResp
     return {"access": access_token}
 
 
-@web_auth_router.post("/token-refresh", response=WebSignInResponse)
+@web_auth_router.post("/token-refresh", response=WebSignInResponse, url_name="web_token_refresh")
 def web_token_refresh(request: HttpRequest) -> dict:
     cookie = request.COOKIES.get(ninja_simple_jwt_settings.JWT_REFRESH_COOKIE_NAME)
     if cookie is None:
