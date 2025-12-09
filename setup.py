feec5f1f83f095ce8f89862b4efeb16b649e5cae
Oscar Chen <oscar.y.chen@gmail.com>
2025-12-08 21:55:32 -0700

    Fix bug raising exception without http status code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # pylint: disable=import-error,E040
 
 setup(
     name="django-ninja-simple-jwt",
-    version="0.7.0",
+    version="0.7.1",
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests", "docs")),
 )
