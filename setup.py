1414daff5eb381b1927761595133d5d0777319cf
Oscar Chen <oscar.y.chen@gmail.com>
2026-03-06 22:43:51 -0700

    Bump package version to 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # pylint: disable=import-error,E040
 
 setup(
     name="django-ninja-simple-jwt",
-    version="0.7.1",
+    version="0.8.0",
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests", "docs")),
 )
